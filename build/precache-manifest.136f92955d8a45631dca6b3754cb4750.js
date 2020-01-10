7d33a21986387d79ef9fbe8c799f269f84baa886
Aleksi Aalto <aleksi.aalto@student.hamk.fi>
2020-01-10 09:21:22 +0200

    commit message

---
+++ build/precache-manifest.136f92955d8a45631dca6b3754cb4750.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90a7f3df73c87b2784767af473a3e86",
+    "url": "/GitHub-Finder/index.html"
+  },
+  {
+    "revision": "d6d377f822e433f51a03",
+    "url": "/GitHub-Finder/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "2d32f397aaf882937f57",
+    "url": "/GitHub-Finder/static/css/main.aca11af2.chunk.css"
+  },
+  {
+    "revision": "d6d377f822e433f51a03",
+    "url": "/GitHub-Finder/static/js/2.4eb4f61e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/GitHub-Finder/static/js/2.4eb4f61e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d32f397aaf882937f57",
+    "url": "/GitHub-Finder/static/js/main.fadad857.chunk.js"
+  },
+  {
+    "revision": "3b4857d3928dbc54c31b",
+    "url": "/GitHub-Finder/static/js/runtime-main.3c3faf37.js"
+  },
+  {
+    "revision": "85fb02352b0167446e71a91a35d4d6cc",
+    "url": "/GitHub-Finder/static/media/Roboto.85fb0235.ttf"
+  },
+  {
+    "revision": "4dbead1e92e31d10e5cf7cc12da270e9",
+    "url": "/GitHub-Finder/static/media/kiikarit.4dbead1e.png"
+  }
+]);
